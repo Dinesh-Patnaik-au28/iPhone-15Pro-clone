188f46680324b34a59a82c61f06879ca9c0cf84e
Dinesh-Patnaik-au28 <dineshpatnaik54@gmail.com>
2024-09-18 00:15:17 +0530

    work going on 3d model rendering

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
+  );
+};
+
+export default App;
